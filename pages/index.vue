c5cf3681daf09e50868afcedddf05e70536dbae3
Po Rith <poamrong@rith.com>
2021-03-06 19:53:06 -0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="d-flex p-5 bg-gradient align-items-center">
+
+  <!-- <div class="bg-white p-5"> -->
+    <div class="container bg-white p-5">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="/wop.png" class="avatar pb-5 pb-md-0 pb-lg-0" alt="">
+        </div>
+        <div class="col-md-8">
+          <b style="font-size: 20px;">WOPONESE_</b><br><br>
+          33 years old, playing live from Pittsburgh, PA.
+          <br><br>
+          Main: COD: Warzone<br>
+          Other: FIFA '21.
+          <br><br>
+          When not online, I could be working on the golf swing at the local park, or hanging and taking keer da kizz. &#60;33
+          <br><br>
+          <a href="https://www.twitch.tv/woponese_">TWITCH</a> <a href="https://www.instagram.com/soister_/">IG</a> <a href="https://www.youtube.com/user/soyster">YOUTUBE</a> <a href="https://discord.gg/8pdbmmwu8T">DISCORD</a> <a href="https://www.paypal.me/bayeezy">DONATE</a>
+        </div>
+      </div>
+    </div>
+    <!-- WOPONESE_ -->
+  <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.avatar {
+  max-width: 300px;
+}
+
+.bg-gradient {
+  width: 100vw;
+  height: 100vh;
+  background: rgb(130,62,233);
+  background: linear-gradient(150deg, rgba(130,62,233,1) 0%, rgba(32,10,57,1) 100%);
+}
+
+.bg-white {
+  background-color: white;
+  border-radius: 30px;
+  margin: auto;
+}
+</style>
