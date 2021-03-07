5c3414500d7da979f55e1edeb83650a79432c663
Po Rith <poamrong@rith.com>
2021-03-06 20:32:11 -0800

    fix img size

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,8 @@ html, body, #__nuxt, #__layout, #__layout > div {
   height: 100%;
 }
 
-body {
+html, body {
+  background-color: rgb(130,62,233);
   background: rgb(130,62,233);
   background: linear-gradient(150deg, rgba(130,62,233,1) 0%, rgba(32,10,57,1) 100%);
 }
