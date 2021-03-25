048fc3d2bb3192b05c2052cf74c9e98c84ba0b1b
Po Rith <poamrong@rith.com>
2021-03-24 21:03:49 -0700

    add follow links

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="d-flex p-5 h-75 align-items-center">
+  <div class="h-100 pb-5">
   <!-- <div class="bg-white p-5"> -->
-    <div class="container bg-white p-5">
-      <div class="row">
-        <div class="col-md-4 text-center">
-          <img src="/wop.png" class="avatar pb-5 pb-md-0 pb-lg-0 w-100" alt="">
+    <div class="d-flex p-5">
+      <div class="container">
+        <div class="row bg-white p-5">
+          <div class="col-md-12">
+            <b style="font-size: 20px;">WOPONESE_</b><br><br>
+          </div>
+          <div class="col-md-4 text-center">
+            <img src="/wop.png" class="avatar pb-5 pb-md-0 pb-lg-0 w-100" alt="">
+          </div>
+          <div class="col-md-8">
+            Gamer going by the handle, Woponese. <br><br>
+            I'm 33 years old, and play live from Pittsburgh, PA.
+            <br><br>
+            Main: COD: Warzone<br>
+            Other: FIFA '21.
+            <br><br>
+            When not online, I could be working on the golf swing at the local park, or hanging and taking keer da kizz. &#60;33
+            <br><br>
+            <a href="https://www.twitch.tv/woponese_">TWITCH</a> <a href="https://www.instagram.com/soister_/">IG</a> <a href="https://www.youtube.com/user/soyster">YOUTUBE</a> <a href="https://discord.gg/8pdbmmwu8T">DISCORD</a> <a href="https://cod.tracker.gg/warzone/profile/atvi/Woponese%233470854/overview">TRACKER.GG</a> <a href="https://www.paypal.me/bayeezy">DONATE</a>
+          </div>
         </div>
-        <div class="col-md-8">
-          <b style="font-size: 20px;">WOPONESE_</b><br><br>
-          33 years old, playing live from Pittsburgh, PA.
-          <br><br>
-          Main: COD: Warzone<br>
-          Other: FIFA '21.
-          <br><br>
-          When not online, I could be working on the golf swing at the local park, or hanging and taking keer da kizz. &#60;33
-          <br><br>
-          <a href="https://www.twitch.tv/woponese_">TWITCH</a> <a href="https://www.instagram.com/soister_/">IG</a> <a href="https://www.youtube.com/user/soyster">YOUTUBE</a> <a href="https://discord.gg/8pdbmmwu8T">DISCORD</a> <a href="https://www.paypal.me/bayeezy">DONATE</a>
+        <div class="row bg-white p-5 mt-5">
+          <div class="col-md-12">
+            <b style="font-size: 20px;">PLAYERS I FOLLOW</b><br><br>
+            <a href="https://www.twitch.tv/swagg" target="_blank"><img src="/avatars/swagg.png" class="avatar-sml" alt=""></a>
+            <a href="https://www.twitch.tv/diazbiffle" target="_blank"><img src="/avatars/biff.png" class="avatar-sml" alt=""></a>
+            <a href="https://www.twitch.tv/gd_booya" target="_blank"><img src="/avatars/booya.png" class="avatar-sml" alt=""></a>
+            <a href="https://www.twitch.tv/bolovlogs" target="_blank"><img src="/avatars/bolo.png" class="avatar-sml" alt=""></a>
+            <a href="https://www.youtube.com/user/DrDisRespect" target="_blank"><img src="/avatars/drdis.png" class="avatar-sml" alt=""></a>
+            <a href="https://www.twitch.tv/its_iron" target="_blank"><img src="/avatars/iron.png" class="avatar-sml" alt=""></a>
+          </div>
         </div>
       </div>
     </div>
-    <!-- WOPONESE_ -->
-  <!-- </div> -->
+    
   </div>
 </template>
 
@@ -32,6 +47,12 @@ export default {
 <style>
 .avatar {
   max-width: 300px;
+}
+
+.avatar-sml {
+  width: 100px;
+  margin-right: 30px;
+  margin-bottom: 30px;
 }
 
 html, body, #__nuxt, #__layout, #__layout > div {
